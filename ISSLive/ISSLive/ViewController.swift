c000cc4075eae4926031a48f94ab6e1b0acb6bd0
Iktan <trejographics@gmail.com>
2022-02-02 12:35:13 -0600

    - Xcode clean project.

---
+++ ISSLive/ISSLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISSLive
+//
+//  Created by Alex on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
